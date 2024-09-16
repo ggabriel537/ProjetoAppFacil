85a345b4a87a47d491cc39c5c1cf9452241434b9
Gabriel <ggabriel667@gmail.com>
2024-09-16 16:30:08 -0400

    Criação de tela de gerenciamento de cardapio

---
+++ src/main/java/br/unigran/views/TelaGerenciarCardapio/TelaGerenciarCardapio.java
@@ -0,0 +1,16 @@
+package br.unigran.views.TelaGerenciarCardapio;
+
+import javax.swing.*;
+
+public class TelaGerenciarCardapio {
+    private JPanel panel1;
+    private JTextField txtItem;
+    private JTextField txtDescricao;
+    private JTextField txtPreco;
+    private JButton btnSair;
+    private JButton btnCadastrarItem;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
